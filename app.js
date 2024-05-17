5226a0926290ba79def536faa9241348941a66e0
SunSpike00 <83578904+SunSpike00@users.noreply.github.com>
2024-05-17 11:45:24 +0900

    socket 적용

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import authRouter from './router/auth.js';
 import bodyParser from 'body-parser';
 import { config } from "./config.js";
 import { connectionDB } from "./db/database.js";
+import { initSocket } from "./connection/socket.js";
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
@@ -15,12 +16,12 @@ app.use(morgan("dev"));
 app.use('/tweets', tweetsRouter);
 app.use('/auth', authRouter);
 
-// app.use((req, res, next) => {
-//     res.sendStatus(404);
-// });
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
 
 connectionDB().then((db) => {
-    console.log('몽구스를 이용하여 몽고디비에 접속 성공!');
-    app.listen(config.host.port);
+    const server = app.listen(config.host.port);
+    initSocket(server);
 }).catch(console.error);
 
